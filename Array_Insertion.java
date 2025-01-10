2c808c15a01da90503550b34caa0b9b48303b8fb
1U23CS055KAVANA <139536059+1U23CS055KAVANA@users.noreply.github.com>
2025-01-10 07:06:23 +0000

    submit

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -18,7 +18,7 @@ public class Array_Insertion {
         arrayInsert(arr, p, e, len);
     }
     static void arrayInsert(int[] arr, int p, int e, int len) {
-        for (int i = len - 1; i >= p - 1; i--) {
+        for (int i = len - 1; i >= (p - 1); i--) {
             arr[i + 1] = arr[i]; 
         }
         arr[p - 1] = e;
@@ -26,7 +26,7 @@ public class Array_Insertion {
         System.out.println("After Inserting");
         System.out.println("Length of the array:"+(len+1));
         for (int i = 0; i < len; i++) {
-            System.out.println("arr["+i+"]=" +arr[i]);
+            System.out.println("arr["+i+"]="+arr[i]);
         }
     }
 }
